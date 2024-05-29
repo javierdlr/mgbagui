8cb0d9386ad17073558e93e8c65ede54f65698f9
Javier de las Rivas <jabierdlr@gmail.com>
2024-05-29 15:17:03 +0200

    amigainput part2
    added amigainput stuff

--- includes/includes.h
+++ includes/includes.h
@@ -13,6 +13,7 @@
 #include <proto/graphics.h>
 #include <proto/icon.h>
 //#include <proto/locale.h>
+#include <proto/amigainput.h>
 
 #include <workbench/icon.h>
 #include <workbench/startup.h>
@@ -44,6 +45,11 @@
 #define SCRSHOTS  "PROGDIR:mGBAConfig/screenshots"
 #define SAVES     "PROGDIR:mGBAConfig/ram"
 
+#define CONFIG_INI "PROGDIR:mGBAConfig/config.ini"
+#define GPAD_IMG "PROGDIR:mGBAConfig/keymap.png"
+#define GPAD_BG  "PROGDIR:mGBAConfig/keymap_bg.png"
+#define GPAD_STR "PROGDIR:mGBAConfig/keymap_str.png"
+
 // gui_build.c: 'make_chooser_list2(<mode>,..)'
 #define ADD_LIST 0
 #define NEW_LIST 1
@@ -53,6 +59,12 @@ enum {
  COL_ROM = 0,
  COL_FMT, // file extension
  LAST_COL
+};
+
+enum {
+ WID_MAIN = 0,
+ WID_GPAD,
+ LAST_WIN
 };
 
 enum {
@@ -69,16 +81,31 @@ enum {
  OID_GFXSCALE,
  OID_FULLSCR,
  OID_FRAMESKIP,
-OID_FORCESIZE_GROUP,
-OID_FORCESIZE,
-OID_FS_WIDTH,
-OID_FS_HEIGHT,
+OID_GAMEPAD_BTN,
+ OID_FORCESIZE_GROUP,
+ OID_FORCESIZE,
+ OID_FS_WIDTH,
+ OID_FS_HEIGHT,
  OID_TOTALROMS,
+// Gamepad window and buttons
+OID_GPAD,
+OID_GPAD_NO_BTN = OID_GPAD,
+OID_GPAD_L,
+OID_GPAD_R,
+OID_GPAD_B,
+OID_GPAD_A,
+OID_GPAD_SEL,
+OID_GPAD_STA,
+OID_GPAD_TEXT,
+OID_GPAD_IMG,
+OID_GPAD_STR,
+OID_GPAD_SAVE,
+OID_GPAD_CANCEL,
  // Buttons
  OID_ABOUT,
  OID_SAVE,
  OID_QUIT,
- LAST_NUM
+ LAST_OID
 };
 
 // Listbrowser's pens
@@ -97,7 +124,7 @@ struct myToolTypes {
 	BOOL show_hints;    // SHOW_HINTS
 	//STRPTR newttp, ttpBuf1, ttpBuf2; // only needed if using SaveToolType()
 	BOOL guifade;       // NO_GUI_FADE
-	BOOL autosnapshot;  // AUTO_SNAPSHOT
+	//BOOL autosnapshot;  // AUTO_SNAPSHOT
 	int32 forcesize_w, forcesize_h; // FORCESIZE_W and FORCESIZE_H
 	int32 viewmode;     // VIEWMODE
 };
@@ -106,18 +133,22 @@ struct myToolTypes {
 #define DISABLE_TT_CHAR '*' // disable tooltype: tooltype[=value] -> (tooltype[=value])
 
 #define FORCESIZE_W_MIN 480
-#define FORCESIZE_H_MIN 432
+#define FORCESIZE_H_MIN 320
 
+#define CFG_FILE_SIZE 4096 // max. size of config.ini in memory (array)
 
 struct MGBAGUI {
 	struct Screen *screen; // PUSBCREEN=<screen_name>
 	struct DiskObject *iconify;
 	struct List *romlist;
-	struct Window *win;
+	struct Window *win[LAST_WIN];
 	struct myToolTypes myTT;
 	struct WBStartup *wbs;
 	struct List *savestates_list;//, *game_opts_list;
 	BOOL forcesize;
+STRPTR cfg_file;
+int32 keyV[6]; // gamepad buttons: L R B A Select Start
+struct MsgPort *ai_port; // amigainput message port
 };
 
 
